965e90864a96597e9a478522f7a70d88b0117a96
Ryan Airth <Ryan22_is@hotmail.com>
2016-08-30 20:31:06 +0100

    Added png versions of icons for use instead of svg due to issues with free weebly site loading resources. Finalized design, few things left to develop if needed.

--- scripts/singlePageScript.js
+++ scripts/singlePageScript.js
@@ -1,6 +1,65 @@
-$(window).ready(function() {
-	loadBackArrow();
-	loadFollowButtons();
+$(document).ready(function() {
+	// Try and load SVGs, if fail load PNGs if fail, use ugly buttons or something to maintain functionality
+	/*try {
+		console.log("Setting up filters");
+
+		setUpSVGFilters();
+
+		console.log("Filters set up successfully");
+
+		try {
+			console.log("Loading SVG social buttons");
+
+			loadFollowButtonsSVG();	
+
+			console.log("SVG social buttons loaded successfully");
+		} catch(err) {
+			console.log(err);
+			console.log("Failed to load SVG social buttons");
+			try {
+				console.log("Loading PNG social buttons");
+
+				loadFollowButtonsPNG();
+
+				console.log("PNG social buttons loaded successfully");
+			} catch(err) {
+				console.log(err);
+				console.log("Failed to load PNG social buttons");
+			}
+		}
+		try {
+			console.log("Loading SVG back arrow button");
+
+			loadBackArrowSVG();
+
+			console.log("SVG back arrow button loaded successfully");
+		} catch(err) {
+			console.log(err);
+			console.log("Failed to load SVG back arrow button");
+			try {
+				console.log("Loading PNG back arrow button");
+
+				loadBackArrowPNG();
+
+				console.log("PNG back arrow button loaded successfully");
+			} catch(err) {
+				console.log(err);
+				console.log("Failed to load back arrow button");
+			}
+		}
+	} catch(err) {
+		try {
+			loadFollowButtonsPNG();
+		} catch(err) {
+
+		}
+		try {
+			loadBackArrowPNG();
+		} catch(err) {
+			
+		}
+	}*/
+	
 	setUpButtons();
 	setUpProjectButtons();
 	setUpGalleryButtons();
@@ -11,51 +70,22 @@ $(window).resize(function() {
 	resizeBackgroundImage();
 });
 
-function loadBackArrow() {
-	var svgId = "#back-arrow";
-	var draw = Snap(svgId);
-	Snap.load("images/SVG/ArrowSmall.svg", function(fragment) {
-		draw.append(fragment);
-
-		var button = Snap.selectAll("#arrow-svg > .svg-button"),
-			scale = 1,
-			timeUp = 150,
-			timeDown = 250,
-			transformUp = "t0,0s" + (scale + 0.1),
-			transformDown = "t0,0s" + scale;	
-
-		button[0].hover(function(event) {
-			var shadow = Snap($("#follow-buttons")[0]).selectAll("filter")[0];
-			
-			this.animate({transform: transformUp}, timeUp);
-			this.attr({filter: shadow});
-
-			Snap.animate(0, 100, function(value) {animateShadow(value, shadow)}, timeUp);
-		}, function(event) {
-			var shadow = Snap($("#follow-buttons")[0]).selectAll("filter")[0],
-			noShadow = Snap($("#follow-buttons")[0]).selectAll("filter")[1];
-
-			this.animate({transform: transformDown}, timeDown, mina.bounce);
-
-			var element = this;
-			Snap.animate(100, 0, function(value) {animateShadow(value, shadow)}, timeDown, mina.bounce, function() {
-				element.attr({filter: noShadow});
-			});
-		});
-	});
+function setUpSVGFilters() {
+	var draw = Snap("#filters");
+	draw.filter(Snap.filter.shadow(0, 0, -1, 3));
+	draw.filter(Snap.filter.shadow(0, 0, -1, 0));
 }
 
-function loadFollowButtons() {
+function loadFollowButtonsSVG() {
 	// Main SVG drawing area
 	var svgId = "#follow-buttons",
 		draw = Snap(svgId);
-	// Shadow filters, one with and one without
-	draw.filter(Snap.filter.shadow(0, 0, -1, 3));
-	draw.filter(Snap.filter.shadow(0, 0, -1, 0));
 	// Filenames
-	var followButtons = Snap.load("images/SVG/FollowButtonsSmall.svg", function(fragment) {
+	var followButtons = Snap.load("images/SVG/FollowButtonsSmall1.svg", function(fragment) {
 		//draw.append(fragment);	
 		draw.append(fragment);
+		/*draw.filter(Snap.filter.shadow(0, 0, -1, 3));
+		draw.filter(Snap.filter.shadow(0, 0, -1, 0));*/
 
 		var buttons = Snap.selectAll("#follow-buttons-svg > .svg-button"),
 			scale = 1,
@@ -68,15 +98,15 @@ function loadFollowButtons() {
 			buttons[i].transform("t0,0s" + scale);
 			//Snap animation values: from, to, setter, duration, [easing], [callback]
 			buttons[i].hover(function(event) {
-				var shadow = Snap($(svgId)[0]).selectAll("filter")[0];
+				var shadow = Snap($("#filters")[0]).selectAll("filter")[0];
 				
 				this.animate({transform: transformUp}, timeUp);
 				this.attr({filter: shadow});
 
 				Snap.animate(0, 100, function(value) {animateShadow(value, shadow)}, timeUp);
 			}, function(event) {
-				var shadow = Snap($(svgId)[0]).selectAll("filter")[0],
-				noShadow = Snap($(svgId)[0]).selectAll("filter")[1];
+				var shadow = Snap($("#filters")[0]).selectAll("filter")[0],
+				noShadow = Snap($("#filters")[0]).selectAll("filter")[1];
 
 				this.animate({transform: transformDown}, timeDown, mina.bounce);
 
@@ -87,6 +117,140 @@ function loadFollowButtons() {
 			});
 		}	
 	});
+
+	$("#follow-buttons").appendTo($(".follow-buttons-container"));
+}
+
+function loadFollowButtonsPNG() {
+	var innerContainer = document.createElement("div");
+	$(innerContainer).addClass("follow-buttons-inner-container").appendTo($(".follow-buttons-container"));
+	// no button container for buutons so can scale perfectly
+	var blogB = document.createElement("div"),
+		githubB = document.createElement("div"),
+		linkedInB = document.createElement("div"),
+		twitterB = document.createElement("div"),
+		googleB = document.createElement("div");
+
+		$(blogB).attr({
+			"id": "blog-button"
+		}).addClass("social-button-png").appendTo($(".follow-buttons-inner-container"));
+		$(githubB).attr({
+			"id": "github-button"
+		}).addClass("social-button-png").appendTo($(".follow-buttons-inner-container"));
+		$(linkedInB).attr({
+			"id": "linked-in-button"
+		}).addClass("social-button-png").appendTo($(".follow-buttons-inner-container"));
+		$(twitterB).attr({
+			"id": "twitter-button"
+		}).addClass("social-button-png").appendTo($(".follow-buttons-inner-container"));
+		$(googleB).attr({
+			"id": "google-plus-button"
+		}).addClass("social-button-png").appendTo($(".follow-buttons-inner-container"));
+
+
+	var blogL = document.createElement("a"),
+		githubL = document.createElement("a"),
+		linkedInL = document.createElement("a"),
+		twitterL = document.createElement("a"),
+		googleL = document.createElement("a");
+
+		$(blogL).attr({
+			"id":"blog-link",
+			"href":"https://ryanairth.blogspot.ie/"
+		}).addClass("link").appendTo($("#blog-button"));
+		$(githubL).attr({
+			"id":"github-link",
+			"href":"https://github.com/bogieman987"
+		}).addClass("link").appendTo($("#github-button"));
+		$(linkedInL).attr({
+			"id":"linked-in-link",
+			"href":"https://www.linkedin.com/in/RyanAirth"
+		}).addClass("link").appendTo($("#linked-in-button"));
+		$(twitterL).attr({
+			"id":"twitter-link",
+			"href":"https://twitter.com/bogieman987"
+		}).addClass("link").appendTo($("#twitter-button"));
+		$(googleL).attr({
+			"id":"google-plus-link",
+			"href":"https://plus.google.com/u/0/+RyanAirth987"
+		}).addClass("link").appendTo($("#google-plus-button"));
+
+	var blog = document.createElement("img"),
+		github = document.createElement("img"),
+		linkedIn = document.createElement("img"),
+		twitter = document.createElement("img"),
+		google = document.createElement("img");
+
+		$(blog).attr({
+			"src":"images/Buttons/SocialButtonsBlog.png",
+			"alt":"Blog Icon"
+		}).addClass("social-button-icon").appendTo($("#blog-link"));
+		$(github).attr({
+			"src":"images/Buttons/SocialButtonsGithub.png",
+			"alt":"Github Logo"
+		}).addClass("social-button-icon").appendTo($("#github-link"));
+		$(linkedIn).attr({
+			"src":"images/Buttons/SocialButtonsLinkedIn.png",
+			"alt":"LinkedIn Logo"
+		}).addClass("social-button-icon").appendTo($("#linked-in-link"));
+		$(twitter).attr({
+			"src":"images/Buttons/SocialButtonsTwitter.png",
+			"alt":"Twitter Logo"
+		}).addClass("social-button-icon").appendTo($("#twitter-link"));
+		$(google).attr({
+			"src":"images/Buttons/SocialButtonsGooglePlus.png",
+			"alt":"Google Plus Logo"
+		}).addClass("social-button-icon").appendTo($("#google-plus-link"));
+}
+
+function loadBackArrowSVG() {
+	var svgId = "#back-arrow";
+	var draw = Snap(svgId);
+	Snap.load("images/SVG/ArrowSmall1.svg", function(fragment) {
+		draw.append(fragment);
+
+		var button = Snap.selectAll("#arrow-svg > .svg-button"),
+			scale = 1,
+			timeUp = 150,
+			timeDown = 250,
+			transformUp = "t0,0s" + (scale + 0.1),
+			transformDown = "t0,0s" + scale;	
+
+		button[0].hover(function(event) {
+			var shadow = Snap($("#filters")[0]).selectAll("filter")[0];
+			
+			this.animate({transform: transformUp}, timeUp);
+			this.attr({filter: shadow});
+
+			Snap.animate(0, 100, function(value) {animateShadow(value, shadow)}, timeUp);
+		}, function(event) {
+			var shadow = Snap($("#filters")[0]).selectAll("filter")[0],
+			noShadow = Snap($("#filters")[0]).selectAll("filter")[1];
+
+			this.animate({transform: transformDown}, timeDown, mina.bounce);
+
+			var element = this;
+			Snap.animate(100, 0, function(value) {animateShadow(value, shadow)}, timeDown, mina.bounce, function() {
+				element.attr({filter: noShadow});
+			});
+		});
+	});
+
+	$("#back-arrow").css({"width": "64px", "height": "64px", "float": "left"}).appendTo($(".project-back-arrow-container"));
+}
+
+function loadBackArrowPNG() {
+	var backArrowContainer = document.createElement("div");
+
+
+
+	var backArrow = document.createElement("img");
+
+	$(backArrow).attr({
+		"id": "back-arrow",
+		"src":"images/Buttons/ArrowLeft.png",
+		"alt":"Link to blog"
+	}).addClass("project-back-arrow").appendTo($(".project-back-arrow-container"));
 }
 
 function animatePortfolioContentChange() {
@@ -194,7 +358,7 @@ function setUpProjectButtons() {
 		onProjectPreviewClick(event, $(this));
 	});
 
-	var backArrow = $("#back-arrow");
+	var backArrow = $("#back-arrow, .project-back-arrow");
 	backArrow.on("click", function(event) {
 		onBackArrowClick(event, $(this));
 	});
